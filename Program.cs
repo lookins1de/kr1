afb0b7d82227eca0f24e550d45f6c03bae0ff201
Alexander <bl055@rambler.ru>
2024-04-18 00:27:19 +0300

    настроили заполнение с разделителями

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] workArray = FillArray();
+string[] resultArray = GenerateNewArray(workArray);
+string firstArray = PrintArray(workArray);
+string secondArray = PrintArray(resultArray);
+Console.WriteLine(firstArray + " -> " + secondArray);
+
+
+string[] FillArray()
+{
+    Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
+    string? enterSymbols = Console.ReadLine();
+    if (enterSymbols == null) { enterSymbols = ""; };
+    char[] separators = new char[] { ',', ' ' };
+    string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return workArray;
+}
+
